d7efc335fa6f6c7daa8c116d5930f4eb6eb190b0
Curtis Charles <ccharles2388@gmail.com>
2019-08-23 11:20:45 -0400

    update functions to record wins and losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,45 @@
 // Creates an array that lists out all of the options (Alphabet Letters).
 var Letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var guessesLeft = 9;
-var UserLetterSelection=null;
-
+var UserLetterSelection = null;
+var ComputerChoice;
+var wins = 0;
+var losses = 0;
+function updatewins() {
+    document.querySelector("#wins").innerHTML = wins;
+}
+function updatelosses() {
+    document.querySelector("#losses").innerHTML = losses;
+}
 function updateGuessesLeft() {
     document.querySelector("#guesses_left").innerHTML = guessesLeft;
 }
 
 function chooseletter() {
-    var randomNumber =  Math.floor((Math.random() * 25) + 1)
-    var ComputerChoice = Letters[randomNumber]
-   console.log(ComputerChoice);  
+    var randomNumber = Math.floor((Math.random() * 25) + 1)
+    ComputerChoice = Letters[randomNumber]
+    console.log(ComputerChoice);
 }
-document.onkeydown = function(event) {
- guessesLeft--;
- console.log(guessesLeft);   
+
+document.onkeydown = function (event) {
+    guessesLeft--;
+    console.log(guessesLeft);
+    updateGuessesLeft();
+    var userKey = event.key.toLowerCase();
+    if (ComputerChoice == userKey) {
+        wins++;
+        updatewins();
+        chooseletter();
+
+    } else {
+
+        losses++;
+        updatelosses();
+    }
 }
 
 // Creating variables to hold the number of wins and losses They start at 0.
-var wins = 0;
-var losses = 0;
+
 
 updateGuessesLeft()
 chooseletter()
